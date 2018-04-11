a5c8d2565e842b48f41b0a18372544d271e62be9
Samantha Bell <sbell093@yahoo.com>
2018-04-10 19:52:08 -0700

    added timer function, appended questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,153 @@
-var page = $("#questions");
+var page = $("#quiz-area");
 
-//holds value for counter
-var timer;
-var unanswered = 0;
-var question = 0;
-
-//Variable and object where all trivia questions are held
 var triviaSet = [{
   question: "Where was Salvador Dali born? ",
   choices: ["France", "Germany", "Spain", "Portugal"],
   validAnswer: "Spain"
 },
-
 {
   question: "In what year was Salvador Dali born?",
   choices: ["1904", "1987", "1910", "1820"],
   validAnswer: "1904"
 },
-
 {
   question: "Which of these most accurately describes Dali's style of art?",
   choices: ["Cubism", "Abstract", "Modernism", "Surrealism"],
   validAnswer: "Surrealism"
 },
-
 {
   question: "Which vegetable did Dali find deep meaning in?",
   choices: ["Carrots", "Kale", "Cauliflower", "Beets"],
   validAnswer: "Cauliflower"
-},
-];
+}];
 
-//entire game
-var trivia = {
-  correctAnswer: 0,
-  incorrectAnswer: 0,
+var timer; 
 
-  //Play Function
-  play: function () {
-    $("#counter").prepend("<h2> Time Left: <span id= 'counter'>100</span> Seconds</h2>");
+var game = {
+
+  correct: 0,
+  incorrect: 0,
+  counter: 120,
+
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("Time up");
+      game.done();
+    }
+  },
+
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+
+    $("#questions").prepend("<h2>Time remaining: <span id='counter-number'>120</span> Seconds</h2>");
+
+    $("#play").remove();
 
     for (var i = 0; i < triviaSet.length; i++) {
       page.append("<h2>" + triviaSet[i].question + "</h2>");
+      for (var j = 0; j < triviaSet[i].choices.length; j++) {
+        page.append("<input type='radio' name='question-" + i + "' value='" + triviaSet[i].choices[j] + "''>" + triviaSet[i].choices[j]);
+      }
     }
-  }
-}
+    page.append("<button id='done'>Done</button>");
+  },
 
-//Timer countdown
-//from jenny...resultCalculator is her var// set timer in a wrapping function
+  done: function () {
 
-timer = setInterval(function () {
-  count: 10,
-  
-  $("#timer").html(count--);
-  if (count == -1) {
+    $.each($("input[name='question-0']:checked"), function () {
+      if ($(this).val() === triviaSet[0].validAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-1']:checked"), function () {
+      if ($(this).val() === triviaSet[1].validAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function () {
+      if ($(this).val() === triviaSet[2].validAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-3']:checked"), function () {
+      if ($(this).val() === triviaSet[3].validAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+},
+
+  result: function () {
+
     clearInterval(timer);
-    resultCalculator();
+
+    $("#questions h2").remove();
+
+    page.html("<h2>All Done!</h2>");
+    page.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    page.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    page.append("<h3>Unanswered: " + (triviaSet.length - (this.incorrect + this.correct)) + "</h3>");
   }
-});
+};
 
+// CLICK EVENTS
 
-
-
-
-
-//On Click buttons
 $(document).on("click", "#play", function () {
-  game.play();
+  game.start();
 });
+
+
 $(document).on("click", "#done", function () {
   game.done();
 });
+
+
+
+// $("#play").click(function () {
+//   $(this).hide();
+//   displayTrivia();
+// }); 
+
+//   var game = {
+
+//     triviaSet: triviaSet,
+//     currentQuestion: 0,
+//     counter: countStartNumber,
+//     correct: 0,
+//     incorrect: 0,
+//   };
+
+// function displayTrivia() {
+//   displayQuestions.html("<h2>" + triviaSet[this.currentQuestion].triviaSet + "</h2>");
+//   question++;
+
+//   var choicesArr = triviaSet[0].choices;
+//   var buttonsArr = [];
+
+//   for (let i = 0; i < choicesArr.length; i++) {
+//     var button = $('<button>');
+//     button.text(choicesArr[i]);
+//     button.attr('data-id', i);
+//     $('#choices_div').append(button);
+//   }
+
+// } 
+// });
